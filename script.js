8d161358036f6b4a1a0398c9e62fde073f965341
Chinatu Lucia Eke <chinatuluciaeke@gmail.com>
2022-06-23 08:39:46 +0100

    modify calculator function

--- script.js
+++ script.js
@@ -15,27 +15,32 @@ function division(x, y) {
 }
 
 function inputNumber() {
-    var x = parseInt(prompt("Enter a number"));
-    var y = parseInt(prompt("Enter another number"));
-    var operation = prompt("Choose an operation");
-    var result;
-    switch (operation) {
-        case "+":
-            result = addition(x, y);
-            break;
-        case "-":
-            result = subtraction(x, y);
-            break;
-        case "*":
-            result = multiplication(x, y);
-            break;
-        case "/":
-            result = division(x, y);
-            break;
-        default:
-            alert("Invalid operation");
+    var x = parseFloat(prompt("Enter a number"));
+    var y = parseFloat(prompt("Enter another number"));
+    var operation = prompt("Choose an operation: '+' or '-' or '*' or '/'");
+
+    if (isNaN(x) == true || isNaN(y) == true) {
+        alert("One or both of your inputs was not a number");
+    } else {
+        var result;
+        switch (operation) {
+            case "+":
+                result = addition(x, y);
+                break;
+            case "-":
+                result = subtraction(x, y);
+                break;
+            case "*":
+                result = multiplication(x, y);
+                break;
+            case "/":
+                result = division(x, y);
+                break;
+            default:
+                alert("Invalid operation");
+        }
+        alert(result);
     }
-    alert(result);
 }
 
 inputNumber();
